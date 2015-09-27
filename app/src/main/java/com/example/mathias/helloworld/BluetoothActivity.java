55c792f660cedec1f2bbfa34c969ac4d9e62f4de
Martin Hansen <night5142@gmail.com>
2015-09-27 22:31:11 +0200

    Added a java file for handling the activity for bluetooth

--- app/src/main/java/com/example/mathias/helloworld/BluetoothActivity.java
+++ app/src/main/java/com/example/mathias/helloworld/BluetoothActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Adapter;
 
 /**
  * Created by martin Hansen on 27-09-2015.
@@ -17,5 +18,24 @@ public class BluetoothActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        onCreateBluetoothAdapter(mBluetoothAdapter);
+    }
+
+    private void onCreateBluetoothAdapter(BluetoothAdapter adapter) {
+        if(adapter == null) {
+            //Device does not support bluetooth activity
+            return;
+        }
+        else if(adapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 120);
+            // Request users to enable bluetooth and bluetooth discoverability if not already enabled
+        }
+        else {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+            startActivity(discoverableIntent);
+        }
     }
 }
