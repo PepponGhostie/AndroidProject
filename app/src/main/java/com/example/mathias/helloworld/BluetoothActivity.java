15fa91f40355fdf70bea06511a6469714635b998
Martin H <night5142@gmail.com>
2015-09-28 15:05:49 +0200

    Added a java file for handling the bluetooth activity

--- app/src/main/java/com/example/mathias/helloworld/BluetoothActivity.java
+++ app/src/main/java/com/example/mathias/helloworld/BluetoothActivity.java
@@ -1,41 +1,51 @@
 package com.example.mathias.helloworld;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by martin Hansen on 27-09-2015.
  */
-public class BluetoothActivity extends Activity {
+public class BluetoothActivity extends Activity implements DeviceList{
     private Activity thisActivity = this;
-    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothAdapter mBthAdapter;
+    public static int REQUEST_BLUETOOTH = 1;
+    protected ArrayList<DeviceList> deviceItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        onCreateBluetoothAdapter(mBluetoothAdapter);
+        BluetoothAdapter mBthAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBthAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support bluetooth")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                    }})
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        if (!mBthAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+        }
     }
 
-    private void onCreateBluetoothAdapter(BluetoothAdapter adapter) {
-        if(adapter == null) {
-            //Device does not support bluetooth activity
-            return;
-        }
-        else if(adapter.isEnabled()){
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 120);
-            // Request users to enable bluetooth and bluetooth discoverability if not already enabled
-        }
-        else {
-            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
-            startActivity(discoverableIntent);
-        }
+    public void onCreateDeviceList(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("DEVICELIST", "Super called for DeviceListFragment onCreate\n");
+        deviceItemList = new ArrayList<DeviceItem>();
+
     }
 }
